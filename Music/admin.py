e7b6332502d9d9b514290dc6c99f2a6d1620071e
Shyam Kumar <svshyam111@gmail.com>
2020-02-21 14:26:58 +0530

    Added register model in admin portal

--- Music/admin.py
+++ Music/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Artist, Album, Tracks
+from .models import Artist, Album, Tracks, Register
 
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Tracks)
+admin.site.register(Register)
