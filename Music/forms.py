1cc5bfd83aaa1f5171afdd919b7109371e9619f9
Shyam Kumar <svshyam111@gmail.com>
2020-02-21 14:31:01 +0530

    Added Account and LogIn class

---
+++ Music/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+
+class Account(forms.Form):
+    firstname = forms.CharField(label="Fist Name", max_length=50)
+    lastname = forms.CharField(label="Last Name", max_length=50)
+    email = forms.EmailField(label="Email", max_length=50)
+    username = forms.CharField(label="Username", max_length=20)
+    password = forms.CharField(
+        label="Password", max_length=30, widget=forms.PasswordInput)
+    mobileNumber = forms.IntegerField(label="Mobile Number")
+    age = forms.IntegerField(label="Age")
+
+
+class Login(forms.Form):
+    username = forms.CharField(label="Username", max_length=50)
+    password = forms.CharField(
+        label="Password", max_length=30, widget=forms.PasswordInput)
