6aa174a31a1d6e39bac9822858d46b1802da7976
Shyam Kumar <svshyam111@gmail.com>
2020-02-26 14:18:22 +0530

    Added MEDIA_URL and MEDIA_ROOT for uploading files.

--- music/urls.py
+++ music/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include,url
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	# /
@@ -26,4 +28,4 @@ urlpatterns = [
 
 	# /admin/
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
