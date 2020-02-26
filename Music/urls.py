caf8291ef72b6348f99d445df036c54bb9b312e4
Shyam Kumar <svshyam111@gmail.com>
2020-02-26 12:20:00 +0530

    Added MEDIA_URL and MEDIA_ROOT for file upload

--- Music/urls.py
+++ Music/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 app_name = "Music"
+
 
 urlpatterns = [
 	# /music/
@@ -38,3 +41,8 @@ urlpatterns = [
 	# /music/profile/
 	path('profile', views.profile, name="profile"),
 ]
+
+
+# For uploading files/images
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
